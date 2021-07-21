52f50e21974ccfb71bb5d05a3b65bcef1db1a1b7
Marla Schulz <marlaa.schulz@gmail.com>
2021-07-21 10:34:18 -0600

    Add status validation default

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -4,12 +4,21 @@ class Applicant < ApplicationRecord
   validates :city, presence: true
   validates :state, presence: true
   validates :zip_code, presence: true
+  validate :status_progress, on: :create
   has_many :pet_applicants
   has_many :pets, through: :pet_applicants
 
   def associated_pets(applicant_id)
     PetApplicant.where('applicant_id = ?', applicant_id).map do |join|
       Pet.find(join.pet_id)
+    end
+  end
+
+  def status_progress
+    if status.present?
+
+    else
+      self.status = 'In Progress'
     end
   end
 end
--- spec/models/applicant_spec.rb
+++ spec/models/applicant_spec.rb
@@ -5,6 +5,19 @@ RSpec.describe Applicant do
     it {should have_many :pet_applicants}
     it {should have_many(:pets).through(:pet_applicants)}
   end
+
+  describe 'validations' do
+    it 'can default a status' do
+      application1 = Applicant.create!(name: 'Carina', street_address: '455 Cool Street', city: 'Portland', state: 'OR', zip_code: 23392, home_description: 'I love my furry friends and have a great yard they can roam around in')
+
+      expect(application1.status).to eq('In Progress')
+
+      application2 = Applicant.create!(name: 'Evan', street_address: '455 Cool Street', city: 'Portland', state: 'OR', zip_code: 23392, home_description: 'Im cool', status: 'Pending')
+
+      expect(application2.status).to eq('Pending')
+    end
+  end
+
   describe 'instance methods' do
     it 'can list associated pet names' do
       furry = Shelter.create!(name:'Furrry Shelter', foster_program: true, city: 'New Orleans', rank: 5)
